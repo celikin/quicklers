5b03e1b07710357505f21eb2608ef923ed21793b
laf341 <laf341@gmail.com>
2015-02-28 07:00:44 +1000

    Added model Customers

---
+++ app/models/Comments.php
@@ -0,0 +1,12 @@
+<?php
+use Illuminate\Database\Eloquent\Model as Eloquent;
+
+class Comments extends Eloquent {
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'comments';
+}
