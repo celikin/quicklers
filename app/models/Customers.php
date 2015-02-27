6b8bb07e50ac9da344a6c3667b4188e0e3cee25d
Daniil Tselikin <dan4ik95dv@gmail.com>
2015-02-28 06:15:40 +1000

    Added model Customers

---
+++ app/models/Customers.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: kun
+ * Date: 28.02.2015
+ * Time: 7:12
+ */
+
+use Illuminate\Database\Eloquent\Model as Eloquent;
+
+class Customers extends Eloquent {
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'customers';
+}
