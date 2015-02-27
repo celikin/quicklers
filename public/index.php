de1d73cf02dc003f04b5a3bd00c1a554ff61b3f2
laf341 <laf341@gmail.com>
2015-02-28 08:39:23 +1000

    add Utills

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
 
 require __DIR__.'/../vendor/autoload.php';
+require __DIR__.'/../public/Utills.php';
 
 $app = new Slim\Slim();
 
@@ -18,13 +19,30 @@ $app->post('/reg', function () use ($app) {
         $user->username = (string)$input->username;
         $user->phone = (string)$input->phone;
         $user->address = (string)$input->address;
-        $user->hash = (string) md5((string)$input->phone);
+        $user->hash = (string) md5(md5((string)$input->phone).mktime());
         $user->banned = false;
         $user->deleted = false;
         $user->save();
         // return JSON-encoded response body
         $app->response()->header('Content-Type', 'application/json');
         echo json_encode(array('status' => 'ok'));
+    } catch (Exception $e) {
+        $app->response()->status(400);
+        $app->response()->header('X-Status-Reason', $e->getMessage());
+    }
+});
+
+$app->post('/auth', function () use ($app){
+    try {
+        $request = $app->request();
+        $body = $request->getBody();
+        $input = json_decode($body);
+        $user = new User;
+        if($user->where('phone', '=', $input->phone)->count() != 0){
+            $user->hash = (string) md5(md5((string)$input->phone).mktime());
+        }
+
+
     } catch (Exception $e) {
         $app->response()->status(400);
         $app->response()->header('X-Status-Reason', $e->getMessage());
