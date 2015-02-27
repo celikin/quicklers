f09794988b6951ab2b184a893dcbbef932c0e550
laf341 <laf341@gmail.com>
2015-02-28 07:07:09 +1000

    Add CommentsMigration

---
+++ app/database/migrations/CommentsMigration.php
@@ -0,0 +1,22 @@
+<?php
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+/**
+ * Example migration for use with "novice"
+ */
+class CommentsMigration {
+    function run()
+    {
+        Capsule::schema()->dropIfExists('comments');
+        Capsule::schema()->create('comments', function($table) {
+            $table->increments('id');
+            $table->string('desc');
+            $table->increments('rate');
+            $table->increments('bid_id')->unsigneed();
+            $table->foreign('bid_id')->references('id')->on('bids');
+            $table->increments('user_id')->unsigned();
+            $table->foreign('user_id')->refernces('id')->on('users');
+            $table->timestamps();
+        });
+    }
+} 
