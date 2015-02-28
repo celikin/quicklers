3fa7753f9db360e0ecf4fb748eefdc111cd09e26
Denis Feshchenko <feshenkod@mail.ru>
2015-03-01 02:18:21 +1000

    Вывод кандидатов

--- public/index.php
+++ public/index.php
@@ -239,4 +239,19 @@ $app->post('/bid/performer/add',  function () use ($app) {
     }
 });
 
+
+//Вывод пользователя по ИД
+function PrintUser($id){
+    $user = User::where('id',$id)->first();
+    echo json_encode($user);
+}
+//Вывод кандидатов
+$app->get('bid/performers', function() {
+    $performersbidstack = PerformersBidStack::all();
+    foreach($performersbidstack as $arr) {
+        $uid = Performer::where('id', $arr->performer_id)->user_id;
+        PrintUser($uid);
+    }
+});
+
 $app->run();
