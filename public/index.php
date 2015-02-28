c653904b282ad5b9b6b568d81b0b1d7532951f03
Denis Feshchenko <feshenkod@mail.ru>
2015-02-28 19:05:59 +1000

    Добавление категории и подкатегории

--- public/index.php
+++ public/index.php
@@ -9,7 +9,7 @@ $app->get('/users', function() {
     echo json_encode($users);
 });
 
-$app->post('/reg', function () use ($app) {
+$app->post('/user/add', function () use ($app) {
     try {
         $request = $app->request();
         $body = $request->getBody();
@@ -22,6 +22,45 @@ $app->post('/reg', function () use ($app) {
         $user->banned = false;
         $user->deleted = false;
         $user->save();
+        // return JSON-encoded response body
+        $app->response()->header('Content-Type', 'application/json');
+        echo json_encode(array('status' => 'ok'));
+    } catch (Exception $e) {
+        $app->response()->status(400);
+        $app->response()->header('X-Status-Reason', $e->getMessage());
+    }
+});
+
+//Добавление категории
+$app->post('/category/add', function () use ($app) {
+    try {
+        $request = $app->request();
+        $body = $request->getBody();
+        $input = json_decode($body);
+        $category = new Category;
+        $category->alias = (string)$input->alias;
+        $category->desc = (string)$input->desc;
+        $category->save();
+        // return JSON-encoded response body
+        $app->response()->header('Content-Type', 'application/json');
+        echo json_encode(array('status' => 'ok'));
+    } catch (Exception $e) {
+        $app->response()->status(400);
+        $app->response()->header('X-Status-Reason', $e->getMessage());
+    }
+});
+
+//Добавление подкатегории
+$app->post('/category/sub/add', function () use ($app) {
+    try {
+        $request = $app->request();
+        $body = $request->getBody();
+        $input = json_decode($body);
+        $subcategory = new SubCategory;
+        $subcategory->alias = (string)$input->alias;
+        $subcategory->desc = (string)$input->desc;
+        $subcategory->category_id = (string)$input->category_id;
+        $subcategory->save();
         // return JSON-encoded response body
         $app->response()->header('Content-Type', 'application/json');
         echo json_encode(array('status' => 'ok'));
